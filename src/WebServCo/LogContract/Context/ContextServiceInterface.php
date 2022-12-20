eddd55080df58e6b9231e6cee0fe7473aa990525
Radu Maier <ariademur@webserv.co>
2022-12-20 12:59:04 +0100

    Add code

---
+++ src/WebServCo/LogContract/Context/ContextServiceInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WebServCo\LogContract\Context;
+
+interface ContextServiceInterface
+{
+    /**
+     * Convert context to string.
+     *
+     * Use case: write in a file.
+     *
+     * PSR allows anything in $context, so we use "mixed" as an exception.
+     * phpcs:ignore SlevomatCodingStandard.TypeHints.DisallowMixedTypeHint.DisallowedMixedTypeHint
+     * @param array<int|string,mixed> $context
+     */
+    public function toString(array $context): string;
+}
---
+++ src/WebServCo/LogContract/Filesystem/FilesystemServiceInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WebServCo\LogContract\Filesystem;
+
+/**
+ * Filesystem helper interface.
+ *
+ * Helps to customize log directory structure.
+ */
+interface FilesystemServiceInterface
+{
+    public function getContextFilePath(string $channel, string $logId): string;
+
+    public function getLogFilePath(string $channel): string;
+
+    public function write(string $path, string $data): bool;
+}
